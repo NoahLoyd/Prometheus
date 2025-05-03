4f8e0eb95dad0e4c28b8b1084c4575bb60598137
NoahLoyd <noah.loyd9@gmail.com>
2025-05-02 22:40:17 -0500

    Update tool_manager.py

--- tools/tool_manager.py
+++ tools/tool_manager.py
@@ -1,29 +1,18 @@
-# tool_manager.py
-from tools.calculator import tool as CalculatorTool
-from tools.file_tool import tool as FileTool
-from tools.notepad import tool as NoteTool
-from tools.internet_tool import tool as InternetTool
-from tools.search_tool import tool as SearchTool
-from tools.summarizer_tool import tool as SummarizerTool  # <- make sure this file exists
-from tools.summarize_web_tool import tool as SummarizeWebTool  # <- new web summary tool
-
 class ToolManager:
     def __init__(self):
-        self.tools = [
-            CalculatorTool(),
-            FileTool(),
-            NoteTool(),
-            InternetTool(),
-            SearchTool(),
-            SummarizerTool(),
-            SummarizeWebTool(),  # <- newly added
-        ]
+        self.tools = {}
 
-    def get_tool(self, name):
-        for tool in self.tools:
-            if tool.name == name:
-                return tool
-        return None
+    def register_tool(self, name, func):
+        """Register a new tool with a unique name."""
+        self.tools[name] = func
 
     def list_tools(self):
-        return [tool.name for tool in self.tools]
+        """List all registered tool names."""
+        return list(self.tools.keys())
+
+    def call_tool(self, name, *args, **kwargs):
+        """Call a registered tool by name with given arguments."""
+        if name in self.tools:
+            return self.tools[name](*args, **kwargs)
+        else:
+            return f"Tool '{name}' not found."
