72435b80f454d21209fa5f802cd7b4f3d3fb6c5e
NoahLoyd <noah.loyd9@gmail.com>
2025-05-09 15:11:21 -0500

    Update __init__.py

--- tools/__init__.py
+++ tools/__init__.py
@@ -1,1 +1,8 @@
-from .tool_manager import ToolManager
+def __init__(self, tool_manager, short_term_memory, long_term_memory):
+    self.tool_manager = tool_manager
+    self.short_term_memory = short_term_memory
+    self.long_term_memory = long_term_memory
+    self.goal = None
+    self.plan = []
+    self.tags = []
+    self.llm_planner = LLMPlanner()
